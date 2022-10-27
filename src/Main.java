8cf870cee77e097dce7d63129539346df2913bf0
PDemenkov <guapnik@gmail.com>
2022-10-27 15:50:45 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(checkCredentials("login","password!","password"));
+    }
 
+    public static boolean checkCredentials(String login, String password, String confirmPassword) {
+        try {
+            return CredentialsValidator.validateCredentials(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка при валидации логина " + e.getMessage());
+            return false;
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка при валидации пароля " +e.getMessage());
+            return false;
+        }
     }
 }
